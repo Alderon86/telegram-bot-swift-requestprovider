74bff9b427f3969b39a2213ae4db739fe91543d5
Matteo Piccina <matteo.piccina@gmail.com>
2019-06-06 20:54:37 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# telegram-bot-swift-requestprovider
+
+A description of this package.
---
+++ Sources/TelegramBotSDKRequestProvider/RequestCompletion.swift
@@ -0,0 +1,15 @@
+//
+// RequestCompletion.swift
+//
+// This source file is part of the Telegram Bot SDK for Swift (unofficial).
+//
+// Copyright (c) 2015 - 2016 Andrey Fidrya and the project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See LICENSE.txt for license information
+// See AUTHORS.txt for the list of the project authors
+//
+
+import Foundation
+
+public typealias RequestCompletion = (_ data: Data, _ error: Error?)->()
